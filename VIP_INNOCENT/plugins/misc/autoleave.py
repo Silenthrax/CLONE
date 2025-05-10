76ad9a876092d3948aef42ab04bb16756376b9da
innocypapatop <moonwalahehe@gmail.com>
2025-05-10 19:29:29 +0530

    Update autoleave.py

--- VIP_INNOCENT/plugins/misc/autoleave.py
+++ VIP_INNOCENT/plugins/misc/autoleave.py
@@ -11,7 +11,7 @@ from VIP_INNOCENT.utils.database import get_client, is_active_chat, is_autoend
 
 async def auto_leave():
     if config.AUTO_LEAVING_ASSISTANT:
-        while not await asyncio.sleep(congif.AUTO_LEAVE_ASSISTANT_TIME):
+        while not await asyncio.sleep("900"):
             from VIP_INNOCENT.core.userbot import assistants
 
             for num in assistants:

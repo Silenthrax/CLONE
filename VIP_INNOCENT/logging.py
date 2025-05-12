dbcb73517e9c47fc9b51a64ed141f01337fab8ad
innocypapatop <moonwalahehe@gmail.com>
2025-05-12 10:16:07 +0530

    Update logging.py

--- VIP_INNOCENT/logging.py
+++ VIP_INNOCENT/logging.py
@@ -13,6 +13,8 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+logging.getLogger("apscheduler").setLevel(logging.ERROR)
+logging.getLogger("telethon").setLevel(logging.ERROR)
 
 
 def LOGGER(name: str) -> logging.Logger:

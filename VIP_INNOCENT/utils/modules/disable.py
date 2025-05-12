954608af79238f343c559072fde8ce887f174d7c
innocypapatop <moonwalahehe@gmail.com>
2025-05-12 11:21:57 +0530

    Update disable.py

--- VIP_INNOCENT/utils/modules/disable.py
+++ VIP_INNOCENT/utils/modules/disable.py
@@ -22,12 +22,12 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 if is_module_loaded(FILENAME):
 
 
-    from MukeshRobot.modules.helper_funcs.chat_status import (
+    from VIP_INNOCENT.utils.modules.helper_funcs.chat_status import (
         connection_status,
         is_user_admin,
         user_admin,
     )
-    from MukeshRobot.modules.sql import disable_sql as sql
+    from VIP_INNOCENT.utils.modules.sql import disable_sql as sql
 
     DISABLE_CMDS = []
     DISABLE_OTHER = []

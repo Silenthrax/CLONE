efbe7258c3ea992fee27e71a81582692098f92ab
innocypapatop <moonwalahehe@gmail.com>
2025-05-12 11:12:53 +0530

    Update logging.py

--- VIP_INNOCENT/logging.py
+++ VIP_INNOCENT/logging.py
@@ -41,48 +41,109 @@ if ENV:
 
     API_ID = int(os.environ.get("API_ID", None))
     API_HASH = os.environ.get("API_HASH", None)
-    DB_URI = os.environ.get("DATABASE_URL")
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    WORKERS = int(os.environ.get("WORKERS", 8))
     
+    ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
+    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
+    DB_URI = os.environ.get("DATABASE_URL")
+    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
+    LOAD = os.environ.get("LOAD", "").split()
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
+    NO_LOAD = os.environ.get("NO_LOAD", "").split()
+    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "worldwide_friend_zone")
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
+    BOT_TOKEN = os.environ.get("TOKEN", None)
+    WORKERS = int(os.environ.get("WORKERS", 8))
+
     try:
         OWNER_ID = int(os.environ.get("OWNER_ID", None))
     except ValueError:
         raise Exception("Your OWNER_ID env variable is not a valid integer.")
 
     try:
+        BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS", "").split())
+    except ValueError:
+        raise Exception("Your blacklisted chats list does not contain valid integers.")
+
+    try:
         DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
+        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "2145093972").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
+
+    try:
+        DEMONS = set(int(x) for x in os.environ.get("DEMONS", "").split())
+    except ValueError:
+        raise Exception("Your support users list does not contain valid integers.")
+
+    try:
+        TIGERS = set(int(x) for x in os.environ.get("TIGERS", "").split())
+    except ValueError:
+        raise Exception("Your tiger users list does not contain valid integers.")
+
+    try:
+        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "").split())
+    except ValueError:
+        raise Exception("Your whitelisted users list does not contain valid integers.")
 
 else:
     from config import *
 
     API_ID = Config.API_ID
     API_HASH = Config.API_HASH
+    ALLOW_CHATS = Config.ALLOW_CHATS
+    ALLOW_EXCL = Config.ALLOW_EXCL
     DB_URI = Config.DATABASE_URL
+    DEL_CMDS = Config.DEL_CMDS
+    LOAD = Config.LOAD
     MONGO_DB_URI = Config.MONGO_DB_URI
+    NO_LOAD = Config.NO_LOAD
+    STRICT_GBAN = Config.STRICT_GBAN
     SUPPORT_CHAT = Config.SUPPORT_CHAT
-    BOT_TOKEN = Config.BOT_TOKEN
-    WORKERS = config.WORKERS
+    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
+    BOT_TOKEN = Config.TOKEN
+    WORKERS = Config.WORKERS
+
     try:
         OWNER_ID = int(Config.OWNER_ID)
     except ValueError:
         raise Exception("Your OWNER_ID variable is not a valid integer.")
 
     try:
+        BL_CHATS = set(int(x) for x in Config.BL_CHATS or [])
+    except ValueError:
+        raise Exception("Your blacklisted chats list does not contain valid integers.")
+
+    try:
         DRAGONS = set(int(x) for x in Config.DRAGONS or [])
+        DEV_USERS = set(int(x) for x in Config.DEV_USERS or [])
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
+    try:
+        DEMONS = set(int(x) for x in Config.DEMONS or [])
+    except ValueError:
+        raise Exception("Your support users list does not contain valid integers.")
+
+    try:
+        TIGERS = set(int(x) for x in Config.TIGERS or [])
+    except ValueError:
+        raise Exception("Your tiger users list does not contain valid integers.")
+
+    try:
+        WOLVES = set(int(x) for x in Config.WOLVES or [])
+    except ValueError:
+        raise Exception("Your whitelisted users list does not contain valid integers.")
+
+
 DRAGONS.add(OWNER_ID)
+DEV_USERS.add(OWNER_ID)
+
 
 updater = tg.Updater(BOT_TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient("VIP_INNOCENT", API_ID, API_HASH)
+telethn = TelegramClient("mukesh", API_ID, API_HASH)
 
-pbot = Client("VIP_INNOCENT", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN,in_memory=True)
+pbot = Client("MukeshRobot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN,in_memory=True)
 dispatcher = updater.dispatcher
 aiohttpsession = ClientSession()
 
@@ -92,7 +153,12 @@ BOT_NAME = dispatcher.bot.first_name
 BOT_USERNAME = dispatcher.bot.username
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS) 
+DEV_USERS = list(DEV_USERS)
+WOLVES = list(WOLVES)
+DEMONS = list(DEMONS)
+TIGERS = list(TIGERS)
 
+# Load at end to ensure all prev variables have been set
 from VIP_INNOCENT.utils.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,

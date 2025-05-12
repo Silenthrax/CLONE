839562a2273c1ec7c6dc29107c02bf3e7f77904a
innocypapatop <moonwalahehe@gmail.com>
2025-05-12 10:36:00 +0530

    Update logging.py

--- VIP_INNOCENT/logging.py
+++ VIP_INNOCENT/logging.py
@@ -1,4 +1,13 @@
 import logging
+import os
+import sys
+import time
+import telegram.ext as tg
+from aiohttp import ClientSession
+from pyrogram import Client
+from telethon import TelegramClient
+
+StartTime = time.time()
 
 logging.basicConfig(
     level=logging.INFO,
@@ -19,3 +28,30 @@ logging.getLogger("telethon").setLevel(logging.ERROR)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
+
+if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+    LOGGER.error(
+        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
+    )
+    quit(1)
+
+ENV = bool(os.environ.get("ENV", False))
+
+if ENV:
+
+    API_ID = int(os.environ.get("API_ID", None))
+    API_HASH = os.environ.get("API_HASH", None)
+    DB_URI = os.environ.get("DATABASE_URL")
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+
+    try:
+        OWNER_ID = int(os.environ.get("OWNER_ID", None))
+    except ValueError:
+        raise Exception("Your OWNER_ID env variable is not a valid integer.")
+
+    try:
+        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
+    except ValueError:
+        raise Exception("Your sudo or dev users list does not contain valid integers.")

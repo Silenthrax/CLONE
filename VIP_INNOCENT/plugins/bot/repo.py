8ae89faed8c8228caa1bfcf6dd349e831418d735
hide-sudo <deathhide08@gmail.com>
2025-07-30 06:55:22 +0530

    Update repo.py

--- VIP_INNOCENT/plugins/bot/repo.py
+++ VIP_INNOCENT/plugins/bot/repo.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from VIP_INNOCENT import app
 
-BOT_USERNAME = "NAKSH_X_MUSICBOT"
+BOT_USERNAME = "DEVA_MUSICBOT"
 
 start_txt = """**
 ✪ 𝐊𝐇𝐔𝐃 𝐁𝐀𝐍𝐀 𝐁𝐇𝐎𝐒𝐃𝐈𝐊𝐄 ✪
@@ -21,12 +21,12 @@ async def start(_, msg):
             ],
      
             [
-             InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/its_innocent_boy_2926"),
-             InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/TFW_UPDATES"),
+             InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/btw_deva"),
+             InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/BotzEmpire"),
              ],
      
              [
-             InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/TFW_CHATTING_CLUB"),
+             InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/BotzEmpireSupport"),
              ],
      
               ]

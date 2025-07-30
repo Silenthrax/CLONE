5406da3b796a9fb36d586073bc77f21ef64d9a2d
hide-sudo <deathhide08@gmail.com>
2025-07-30 06:53:58 +0530

    Update mustjoin.py

--- VIP_INNOCENT/plugins/bot/mustjoin.py
+++ VIP_INNOCENT/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from VIP_INNOCENT import app
 
 #--------------------------
 
-MUST_JOIN = "abt_innocent"
+MUST_JOIN = "BotzEmpire"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
